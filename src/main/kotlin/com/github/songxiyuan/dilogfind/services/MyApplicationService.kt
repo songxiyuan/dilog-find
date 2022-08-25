edcafefaf9b2fa7f2731e966ef142e77b3b49a6e
GitHub Action <action@github.com>
2022-08-25 14:31:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/songxiyuan/dilogfind/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/songxiyuan/dilogfind/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.songxiyuan.dilogfind.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.songxiyuan.dilogfind.MyBundle
 
 class MyApplicationService {
 
